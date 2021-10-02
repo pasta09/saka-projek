d54d3d7ceaf0839dcf81de7edd4d991a760bfc56
pasta09 <91801142+pasta09@users.noreply.github.com>
2021-10-03 01:53:02 +0700

    Create saka.php

---
+++ saka.php
@@ -0,0 +1,15 @@
+<?php
+    //OPERATOR ARITMATIKA
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+ 
+    echo "<br/> PENJUMLAHAN <br/>";
+    echo $a+$b; //tercetak 5
+    echo "<br/>";
+    $f = $c+$d;
+    echo $f;        //tercetak 15
+    echo "<br/>";
+ 
+?>
